b1b3e34f9f803ea4555d0023847ed69b3b9083df
bergmandenis753-oss <bergmandenis753@gmail.com>
2026-07-28 16:21:54 +0200

    Wire Telegram bot topup endpoint

--- ops_entry.py
+++ ops_entry.py
@@ -9,6 +9,7 @@ import main_compat
 import multi_active_terminators_patch
 import reserve_balance_patch
 import route_pool_patch
+import telegram_balance_patch
 
 
 app = main_compat.app
@@ -187,3 +188,4 @@ route_pool_patch.install(db)
 reserve_balance_patch.install(app, main, db)
 multi_active_terminators_patch.install(app, main, db)
 client_route_isolation_patch.install(app, main, db)
+telegram_balance_patch.install(app, main, db)
